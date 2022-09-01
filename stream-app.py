7289511d0d6d3403f9d8f1d82091cc6e8fb51af1
slava-max <58883908+slava-max@users.noreply.github.com>
2022-09-01 12:10:33 +0200

    Update stream-app.py
    only selected to display...

--- stream-app.py
+++ stream-app.py
@@ -13,5 +13,8 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Cantaloupe'])
-streamlit.dataframe(my_fruit_list)
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Cantaloupe'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+##streamlit.dataframe(my_fruit_list)
+# dsplay the table on the page
+streamlit.dataframe(fruits_to_show)
