0461e98e9484741af583fa8cd5b4c02f00d781ea
slava-max <58883908+slava-max@users.noreply.github.com>
2022-09-01 10:26:17 +0200

    Update stream-app.py
    as per lesson 3...

--- stream-app.py
+++ stream-app.py
@@ -1,6 +1,9 @@
 import streamlit
 streamlit.title   ('My Parents New Healthy Dinner')
+
 streamlit.header  ('Breakfast Menu')
-streamlit.text    ('Omega 3 & Blueberry Oatmeal')
-streamlit.text    ('Kale, Spinat...')
-streamlit.text    ('Hard-Biled Egg')
+streamlit.text    ('🥣    Omega 3 & Blueberry Oatmeal')
+streamlit.text    ('🥗    Kale, Spinach & Rocket Smoothie')
+streamlit.text    ('🥑🍞  Hard-Boiled Free-Range Egg')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
