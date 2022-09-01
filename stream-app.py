847d4a231a02d586183d083b127c29dceb44893b
slava-max <58883908+slava-max@users.noreply.github.com>
2022-09-01 10:21:48 +0200

    Update stream-app.py
    updated the lines...

--- stream-app.py
+++ stream-app.py
@@ -1,2 +1,6 @@
 import streamlit
-streamlit.title ('My Parents New Healthy Dinner')
+streamlit.title   ('My Parents New Healthy Dinner')
+streamlit.header  ('Breakfast Menu')
+streamlit.text    ('Omega 3 & Blueberry Oatmeal')
+streamlit.text    ('Kale, Spinat...')
+streamlit.text    ('Hard-Biled Egg')
