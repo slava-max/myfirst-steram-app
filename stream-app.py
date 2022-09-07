5d669d2120a7943c642d795230ceca06d75614ef
slava-max <58883908+slava-max@users.noreply.github.com>
2022-09-07 11:03:25 +0200

    Update stream-app.py

--- stream-app.py
+++ stream-app.py
@@ -23,7 +23,7 @@ streamlit.dataframe(fruits_to_show)
 #New section to display fruityvice api response
 streamlit.header  ('Типа совет дня...')
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 # streamlit.text(fruityvice_response.json())
 # get frame for the content... 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
