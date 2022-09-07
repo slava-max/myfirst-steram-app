6cb2a91b09cb7a4f77c97019f1d50a5773a8b2ec
slava-max <58883908+slava-max@users.noreply.github.com>
2022-09-07 11:02:25 +0200

    Update stream-app.py

--- stream-app.py
+++ stream-app.py
@@ -24,8 +24,7 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header  ('Типа совет дня...')
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
-# write your own comment -what does the next line do? 
+# streamlit.text(fruityvice_response.json())
+# get frame for the content... 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-# streamlit.dataframe(fruityvice_normalized)
+streamlit.dataframe(fruityvice_normalized)
