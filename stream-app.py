8900e9fe04a6169a6698aecafdf848c94409765d
slava-max <58883908+slava-max@users.noreply.github.com>
2022-09-07 11:05:23 +0200

    Update stream-app.py

--- stream-app.py
+++ stream-app.py
@@ -22,8 +22,12 @@ streamlit.dataframe(fruits_to_show)
 
 #New section to display fruityvice api response
 streamlit.header  ('Типа совет дня...')
+
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 # streamlit.text(fruityvice_response.json())
 # get frame for the content... 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
