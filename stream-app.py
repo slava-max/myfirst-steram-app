c497b82b41b73b8ccf951a58039092874c404f75
slava-max <58883908+slava-max@users.noreply.github.com>
2022-09-09 18:38:27 +0200

    Update stream-app.py

--- stream-app.py
+++ stream-app.py
@@ -25,19 +25,19 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # dsplay the table on the page
 streamlit.dataframe(fruits_to_show)
 
-#New section to display fruityvice api response
+# New section to display fruityvice api response
 streamlit.header  ('Типа совет дня...')
-
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-# import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-# streamlit.text(fruityvice_response.json())
-# get frame for the content... 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-streamlit.dataframe(fruityvice_normalized)
-
+try:
+fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choise:
+    streamlit.error("Выбери фрукт! Че не понятно то?")
+  else
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
+    
 # do not run anything after that = troubleshooting
 streamlit.stop()
 
