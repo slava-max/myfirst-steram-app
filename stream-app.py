d4e9223e957f63bc6ca1316ba2901839a86f3f31
slava-max <58883908+slava-max@users.noreply.github.com>
2022-09-11 10:13:51 +0200

    Update stream-app.py

--- stream-app.py
+++ stream-app.py
@@ -52,8 +52,7 @@ def get_fruit_load_list():
 
 # Add button to load the fruit
 if streamlit.button('Get Fruil Load List'):
-# import snowflake.connector
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_data_rows = get_fruit_load_list()
 streamlit.dataframe(my_data_rows)
 
