d44e89c114710a216b374a22eb30cf2e50dca8c9
slava-max <58883908+slava-max@users.noreply.github.com>
2022-09-11 09:57:43 +0200

    Update stream-app.py

--- stream-app.py
+++ stream-app.py
@@ -25,6 +25,12 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # dsplay the table on the page
 streamlit.dataframe(fruits_to_show)
 
+# Create repeatable code block (aks function)
+def get_fruityvice_data(this_fruit_chioce):
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized
+
 # New section to display fruityvice api response
 streamlit.header('Типа совет дня...')
 try: 
@@ -32,9 +38,8 @@ try:
     if not fruit_choice: 
         streamlit.error('Выбери фрукт! Че не понятно то?')
     else:
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-        streamlit.dataframe(fruityvice_normalized)
+        back_from_function = get_fruityvice_data(fruit_choice)
+        streamlit.dataframe(back_from_function)
 except URLError as e: 
     streamlit.error()
     
